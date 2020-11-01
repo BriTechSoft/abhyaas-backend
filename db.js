bc9b41406634f1e0d49796526496c12f8e2d11ce
Navya Anand <navya.anand@scudos.eu>
2020-11-01 15:44:47 +0100

    Backend refactor

--- db.js
+++
@@ -1,9 +0,0 @@
-const dbUri = process.env.DB_URI;
-const mongoose = require('mongoose');
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
-
